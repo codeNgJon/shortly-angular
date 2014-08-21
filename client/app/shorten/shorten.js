894436363b75c77f6b6de8fae2e5b191f71f9693
Hack Reactor Students <1jonathan.ng@gmail.com>
2014-08-21 11:09:52 -0700

    Adds client-side validation to sign-in form, sign-up form, and shorten url form.

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -4,7 +4,8 @@ angular.module('shortly.shorten', [])
   // Your code here
   $scope.link = {};
   $scope.loading = false;
-  $scope.validateUrl = 'no error so far, bro.';
+  $scope.validateUrl;
+  $scope.validUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
 
   $scope.addLink = function(){
     $scope.loading = true;
