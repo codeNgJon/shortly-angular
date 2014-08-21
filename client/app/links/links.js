bb867def89e2866f011b5bc3426491c07fbcc5e8
Hack Reactor Students <1jonathan.ng@gmail.com>
2014-08-20 17:08:10 -0700

    Adds links and shorten functionality. Passes 12/14 specs.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,18 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  console.log('in LinksController!');
+  $scope.data = {};
+
+  $scope.getLinks = function() {
+    Links.getLinks()
+      .then(function(resp) {
+        $scope.data = resp.data;
+        console.log('Services links response.data:', resp.data);
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  };
+
 });
